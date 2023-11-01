6f6c01e94ee584936af90076df419eb1c8455b59
M .Saenger <ms@zimmerberg-risk.ch>
2023-11-01 07:06:50 +0000

    2023-11-01b

--- scripts/build.R
+++ scripts/build.R
@@ -23,7 +23,7 @@ devtools::build_vignettes()
 
 # Build and install local
 p <- devtools::build(vignettes = F)
-#detach(name = sprintf("package:%s", pkg.name), character.only = TRUE,  unload = TRUE)
+detach(name = sprintf("package:%s", pkg.name), character.only = TRUE,  unload = TRUE)
 devtools::install_local(p, force = TRUE, upgrade = "never", build_manual = T, build_vignettes = F)
 
 devtools::build_site()
